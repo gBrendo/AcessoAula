393f0fdd10f4bc4da3730a4ccde311aa302beebf
Brendo Gmoes <brendo.carmo@hotmail.com>
2023-04-15 15:21:35 -0300

    new commit

---
+++ Produto.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Produto
+{
+    // private-> atributos e metodos visiveis somente na classe onde foram criados.
+    //protected-> atributos e metodos visiveis em classes onde sao criados ou herdados.
+    //public -> atributos e metodos visiveis em qualquer classe.
+}
